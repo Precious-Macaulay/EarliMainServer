d6e71cd727bfca6b433919b08769a2e7394cfa3b
Precious-Macaulay <preciousmacaulay001@gmail.com>
2022-07-27 18:35:21 +0100

    done

--- controller.js
+++ controller.js
@@ -224,8 +224,7 @@ const populateChildInParents = async (req, res) => {
 //populate savings
 const populateSavingsInChild = async (req, res) => {
   try {
-    console.log(req.params.id)
-    const getSingleChild = await ChildModel.findById(req.params.id).populate(
+    const getSingleChild = await ChildModel.findById(req.params.childid).populate(
       'savings'
     );
     res.status(201).json({ message: 'Child Data', data: getSingleChild });
